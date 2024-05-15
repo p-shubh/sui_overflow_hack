a69839c3a6a008493d863c08a69faf6f1c9f02fa
Shubham Prajapati <shubhamprajapati032@gmail.com>
2024-05-15 19:06:51 +0530

    removing .env

--- main.go
+++ main.go
@@ -2,15 +2,8 @@ package main
 
 import (
 	router "hack/Router"
-	"log"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 	router.HandleRequest()
 }
