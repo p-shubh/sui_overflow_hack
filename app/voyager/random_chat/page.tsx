4e39adfa1b90229bb284552d40e01c82f8bf3180
Shilpa Chauhan <chauhanshilpa602@gmail.com>
2024-05-15 15:59:42 +0530

    Make the  random chat page

--- app/voyager/random_chat/page.tsx
+++ app/voyager/random_chat/page.tsx
@@ -1,12 +1,33 @@
-import Sidebar from '@/app/components/random_chat_components/Sidebar'
-import React from 'react'
+"use client";
 
-const page = () => {
+import { useState } from "react";
+import Sidebar from "@/app/components/random_chat_components/Sidebar";
+
+const RandomChat = () => {
+  const [message, setMessage] = useState<string>("");
+
+  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value);
+    console.log("Message: ", event.target.value);
+  };
+
   return (
-    <div>
+    <div className="flex w-full h-full">
       <Sidebar />
+      <div className="flex h-[100vh] w-full ml-[25%] bg-[#393E46]">
+        <div className="w-[90%] self-end mx-auto mb-5">
+          <input
+            type="text"
+            id="default-input"
+            className="text-gray-50 text-sm rounded-lg block p-2.5 w-full bg-gray-500 outline-none"
+            placeholder="Enter text here..."
+            value={message}
+            onChange={handleMessageChange}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default RandomChat;
