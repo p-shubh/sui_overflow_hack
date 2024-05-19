bae39294f49e9cf4a6e5def7886825cf87b2b5ab
Shubham Prajapati <shubhamprajapati032@gmail.com>
2024-05-19 21:40:30 +0530

    handle cors policy

--- Router/router.go
+++ Router/router.go
@@ -50,7 +50,7 @@ func HandleRequest() {
 	// 	MaxAge:           12 * time.Hour,
 	// }))
 
-	ginApp.Use(CorsMiddleware())
+	ginApp.Use(CORSMiddleware())
 
 	ginApp.Use(static.Serve("/", static.LocalFile("./web", false)))
 
@@ -74,11 +74,12 @@ func ApplyRoutes(r *gin.RouterGroup) {
 	}
 }
 
-func CorsMiddleware() gin.HandlerFunc {
+func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
