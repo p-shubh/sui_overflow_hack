5185f0b1dc95fec351d7dfe0c19831b0ff25f67d
Shilpa Chauhan <chauhanshilpa602@gmail.com>
2024-05-29 12:20:30 +0530

    Give some hardcoded value if some data is null in profile section

--- app/voyager/profile/page.tsx
+++ app/voyager/profile/page.tsx
@@ -32,7 +32,7 @@ const Profile = () => {
   const [isEditProfileClicked, setIsEditProfileClicked] = useState(false);
   const [activeTab, setActiveTab] = useState("Cults");
   const [userData, setUserData] = useState<UserData>();
-
+  
   const IP_ADDRESS = process.env.NEXT_PUBLIC_SERVER_IP_ADDRESS;
 
   const searchParams = useSearchParams();
@@ -102,7 +102,9 @@ const Profile = () => {
           <div className="flex items-center justify-between">
             <div className="flex items-center">
               <h2 className="text-xl font-bold">
-                {userData !== undefined ? userData?.name : "Random-User"}
+                {userData !== undefined && userData.name !== ""
+                  ? userData?.name
+                  : "Random-User"}
               </h2>
               <CiEdit
                 className="text-xl ml-12 cursor-pointer"
@@ -125,11 +127,17 @@ const Profile = () => {
             {/* <span className="text-sm font-semibold text-[#343433]">
               Interest: {userData?.interest}
             </span> */}
+            {/* <span className="text-sm font-semibold text-[#343433]">
+              Age:&nbsp;
+              {userData !== undefined && userData.name !== ""
+                ? userData?.age
+                : "00"}
+            </span> */}
             <span className="text-sm font-semibold text-[#343433]">
-              Age: age
-            </span>
-            <span className="text-sm font-semibold text-[#343433]">
-              Gender: {userData?.gender}
+              Gender:&nbsp;
+              {userData !== undefined && userData.name !== ""
+                ? userData?.gender
+                : "other"}
             </span>
           </div>
           <div className="flex gap-3 mt-5">
@@ -153,7 +161,7 @@ const Profile = () => {
             </div>
           </div>
           <div className="mt-3 text-[#474747] text-md font-medium">
-            User Address: {userAddress}
+            <span className="font-semibold text-sm">User Address:</span>&nbsp;<span>{userAddress}</span>
           </div>
         </div>
       </div>
